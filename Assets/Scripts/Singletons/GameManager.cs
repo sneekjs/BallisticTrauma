4ee92d87a10191f9e722ad8bd5f227a9560d06ea
sneekjs <jorritslaats@gmail.com>
2019-04-07 17:01:59 +0200

    Fix not being able to have a game and unlock manager at the same time

--- Assets/Scripts/Singletons/GameManager.cs
+++ Assets/Scripts/Singletons/GameManager.cs
@@ -5,19 +5,41 @@
     using System.Collections.Generic;
     using UnityEngine;
 
-    public class GameManager : Singleton
+    public class GameManager : MonoBehaviour
     {
         private GameState _gameState;
 
         private int _currentDifficulty = 1;
 
         [SerializeField]
-        private float _sensitivity = 60;        
+        private float _sensitivity = 100;        
+
+        protected static GameManager instance = null;
+
+        public static GameManager Instance
+        {
+            get { return instance; }
+        }
 
         public float Sensitivity
         {
             get { return _sensitivity; }
             set { _sensitivity = value; }
+        }
+
+        void Awake()
+        {
+            if (instance != null && instance != this)
+            {
+
+                Destroy(this.gameObject);
+                return;
+            }
+            else
+            {
+                instance = this;
+            }
+            DontDestroyOnLoad(this.gameObject);
         }
     }
 }
--- Assets/Scripts/Singletons/UnlockManager.cs
+++ Assets/Scripts/Singletons/UnlockManager.cs
@@ -4,12 +4,34 @@
     using System.Collections.Generic;
     using UnityEngine;
 
-    public class UnlockManager : Singleton
+    public class UnlockManager : MonoBehaviour
     {
         //all unlocked and locked upgrades/perks/weapons and other pre-start selectables should be remembered here
 
         private int _totalCurrenty;
 
         private int _currentRun;
+
+        protected static UnlockManager instance = null;
+
+        public static UnlockManager Instance
+        {
+            get { return instance; }
+        }
+
+        void Awake()
+        {
+            if (instance != null && instance != this)
+            {
+
+                Destroy(this.gameObject);
+                return;
+            }
+            else
+            {
+                instance = this;
+            }
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
 }
