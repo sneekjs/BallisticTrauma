0ad9c57f784c4cab1356c11d1c9ce09ce99fab01
sneekjs <jorritslaats@gmail.com>
2019-04-07 19:11:14 +0200

    Add begin to turret classes

---
+++ Assets/Scripts/Actors/StaticEnemies/AutoAimTurret.cs
@@ -0,0 +1,67 @@
+﻿namespace JorritSlaats.BallisticTrauma.Scripts.Actors
+{
+    using JorritSlaats.BallisticTrauma.Scripts.Behaviours;
+    using JorritSlaats.BallisticTrauma.Scripts.Singletons;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class AutoAimTurret : Turret
+    {
+        [SerializeField]
+        private GameObject _moveableTurretHead;
+
+        [SerializeField]
+        private float _turningSpeed;
+
+        private bool _seesTarget = false;
+
+        private GameObject _target;
+
+        protected override void Start()
+        {
+            base.Start();
+            _target = GameManager.Instance.Player.Camera.gameObject;
+        }
+
+        private void Update()
+        {
+            LookForTarget();
+
+            if (_seesTarget)
+            {
+                AimAtTarget(_target.transform.position);
+                ShootAtTarget();
+            }
+            else
+            {
+                AimAtTarget(_moveableTurretHead.transform.position + Vector3.forward);
+            }
+        }
+
+        private void LookForTarget()
+        {
+            RaycastHit hit;
+            var dir = _target.transform.position - _moveableTurretHead.transform.position;
+            Ray myRay = new Ray(_moveableTurretHead.transform.position, dir);
+
+            Debug.DrawRay(_moveableTurretHead.transform.position, dir);
+
+            if (Physics.Raycast(myRay, out hit))
+            {
+                _seesTarget = hit.collider.CompareTag("Player");
+            }
+        }
+
+        private void AimAtTarget(Vector3 target)
+        {
+            Quaternion targetQuaternion = Quaternion.LookRotation(target - _moveableTurretHead.transform.position);
+            _moveableTurretHead.transform.rotation = Quaternion.RotateTowards(_moveableTurretHead.transform.rotation, targetQuaternion, _turningSpeed * Time.deltaTime);
+        }
+
+        private void ShootAtTarget()
+        {
+            //Shoot at target
+        }
+    }
+}
---
+++ Assets/Scripts/Actors/StaticEnemies/Turret.cs
@@ -0,0 +1,16 @@
+﻿namespace JorritSlaats.BallisticTrauma.Scripts.Actors
+{
+    using JorritSlaats.BallisticTrauma.Scripts.Singletons;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Turret : StaticEnemy
+    {
+        [SerializeField]
+        private GameObject _bulletPrefab;
+
+        [SerializeField]
+        private List<GameObject> _barrels = new List<GameObject>();        
+    }
+}
