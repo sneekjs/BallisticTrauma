578911964344246f99dea5172e116df424efe053
sneekjs <jorritslaats@gmail.com>
2019-04-07 17:02:46 +0200

    Add working player movement behaviour

--- Assets/Scripts/Behaviours/Movement/PlayerMovement.cs
+++ Assets/Scripts/Behaviours/Movement/PlayerMovement.cs
@@ -11,9 +11,10 @@
         {
             float x = Input.GetAxis("Vertical");
             float y = Input.GetAxis("Horizontal");
-            Vector3 dir = new Vector3(x, 0, y);
-
-            movingActor.Rigidbody.MovePosition(transform.position + dir * movingActor.Speed * Time.deltaTime);
+            
+            Vector3 pos = movingActor.transform.position + transform.right * y * (movingActor.Speed * Time.deltaTime);
+            pos = pos + transform.forward * x * (movingActor.Speed * Time.deltaTime);
+            movingActor.Rigidbody.MovePosition(pos);
         }
     }
 }
