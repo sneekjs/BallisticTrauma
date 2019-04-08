fac060525788340a616155bf845decbcaaa67386
sneekjs <jorritslaats@gmail.com>
2019-04-08 18:35:57 +0200

    Add default bullet prefab

--- Assets/Scripts/DamageDealer/Bullet.cs
+++ Assets/Scripts/DamageDealer/Bullet.cs
@@ -5,12 +5,32 @@
     using System.Collections.Generic;
     using UnityEngine;
 
-    public class Bullet : MonoBehaviour
+    [RequireComponent(typeof(Rigidbody))]
+    public class Bullet : DamageDealer
     {
         [SerializeField]
         private float _speed;
 
         [SerializeField]
         private DespawnBehaviour _despawnBehaviour;
+
+        private Rigidbody _rigidbody;
+
+        private void Start()
+        {
+            Rigidbody = GetComponent<Rigidbody>();
+        }
+
+        protected float Speed
+        {
+            get { return _speed; }
+            private set { _speed = value; }
+        }
+
+        protected Rigidbody Rigidbody
+        {
+            get { return _rigidbody; }
+            private set { _rigidbody = value; }
+        }
     }
 }
---
+++ Assets/Scripts/DamageDealer/ForwardBullet.cs
@@ -0,0 +1,16 @@
+﻿namespace JorritSlaats.BallisticTrauma.Scripts.DamageDealer
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class ForwardBullet : Bullet
+    {
+        private void Update()
+        {
+            Vector3 pos = transform.position + transform.forward * (Speed * Time.deltaTime);
+            pos = pos + transform.forward * (Speed * Time.deltaTime);
+            Rigidbody.MovePosition(pos);
+        }
+    }
+}
